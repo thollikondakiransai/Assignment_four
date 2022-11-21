099b61521ac14295c7fa2b2e844ba2307ae67bff
thollikondakiransai <thollikondakiransai@gmail.com>
2022-11-20 23:51:07 -0500

    user changes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const User = require('../models/user');
+const User_ = require('../models/user');
 const router = express.Router();
 
 router
   .get('/', async (req, res) => {
     try {
-      const users = await User.getAllUsers();
+      const users = await User_.getAllUsers();
       res.send(users);
     } catch(err) {
       res.status(401).send({message: err.message});
@@ -14,7 +14,7 @@ router
 
   .post('/login', async (req, res) => {
     try {
-      let user = await User.login(req.body);
+      let user = await User_.login(req.body);
       res.send({...user, password: undefined})
     } catch(err) {
       res.status(401).send({message: err.message});
